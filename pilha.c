63fb505a4acb8d9f736f7bdf85816916a2ab29db
lucasLdeo <lucasldeoliveira@gmail.com>
2021-05-24 14:06:57 -0300

    Correcao

--- pilha.c
+++ pilha.c
@@ -3,31 +3,35 @@
 #include <string.h>
 
 int main(){
-    int M,cont =0, valor=0, i;
+    int M,cont =0,i;
     char acoes[15];
     scanf("%d",&M);
-    int * pilha = malloc (M * sizeof( int));    
-    printf("%d",sizeof(pilha));
-    while (sizeof(pilha)< M){
+    int * pilha = malloc (M * sizeof( int));        
+    while (1){
+        
         scanf("%s",acoes);              
         if (acoes[0] == '\0'){
                 break;        
-        }else{    
-            if (strcmp(acoes,"Empilha") == 0){                                
-                pilha[cont] = cont+1;
-                cont = cont+1;                
-            }     
-            if (strcmp(acoes,"Desempilha") == 0){                
-                pilha[cont] = 0;
-                cont = cont - 1;
-            } 
-        }
-        printf("Pilha: ");
-            for(i = 0; i< cont; i++){
-                printf("%d ",pilha[i]);
-            }                        
-        printf("\n");
+        }else{         
+            if (cont < M){       
+                if (strcmp(acoes,"Empilha") == 0){                                
+                    pilha[cont] = cont+1;
+                    cont = cont+1;                            
+                }
+            }else{
+                printf("Pilha Cheia\n");
+            }
+            if (cont > 0){     
+                if (strcmp(acoes,"Desempilha") == 0){                
+                    pilha[cont] = 0;
+                    cont = cont - 1;
+                    printf("Desempilha %d\n", cont+1);
+                } 
+            }else{
+                printf("Pilha Vazia\n");
+            }
         }              
+    }              
         
     return 0;
 }
